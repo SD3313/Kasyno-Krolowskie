19591d074817b1432ae82611471f0c693152b587
SD3313 <dziedzinaszymon1@gmail.com>
2026-05-25 18:38:54 +0200

    first_coomit

---
+++ db_connect.php
@@ -0,0 +1,18 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', ''); 
+define('DB_NAME', 'kasyno_db');
+
+// połączenie z bazą danych
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$conn) {
+    die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+}
+
+
+mysqli_set_charset($conn, "utf8mb4");
+
+//echo "Połączono z bazą danych pomyślnie!";
+?>
